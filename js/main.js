1391866e8a2253d49c49ac0237504cc5f3704f50
iamirsalimi <wonderguy2003@gmail.com>
2022-09-08 15:04:43 +0430

    add and remove books action added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,82 @@
-let inputText = document.querySelector('.add-book input');
-let button = document.querySelectorAll('.button')[1];
-let ul =document.querySelector('ul');
-button.addEventListener('click',function(e){
-    let li = document.createElement('li');
-    let spanName = document.createElement('span');
-    let spanDelete =document.createElement('span');
+// add books to list
+const ul = document.querySelector('ul');
+const link = document.querySelector('.button');
+const inputText = document.querySelector('.add-books input');
+const spanDelete = `<span class="delete">delete</span>`;
+
+
+link.addEventListener('click',(e)=>{
+    const li = document.createElement('li');
+    const spanName = document.createElement('span');
     spanName.className = 'name';
-    spanDelete.className = 'delete';
-    spanDelete.innerHTML = 'delete';
+    spanName.textContent = inputText.value;
     li.appendChild(spanName);
-    li.appendChild(spanDelete);
-    spanName.innerHTML = inputText.value;
-    inputText.value = '';
+    li.innerHTML += spanDelete;
     ul.appendChild(li);
+    storeTOlocalStorage(inputText.value);
+    inputText.value = '';
     e.preventDefault();
 });
-ul.addEventListener('click',function(e){
-    if(e.target.className === 'delete'){
-        e.target.parentElement.remove();     
+// get books from local storage
+document.addEventListener('DOMContentLoaded' ,(e)=>{
+    let books;
+    if(localStorage.getItem('books') === null){
+        books =[];
+    } else {
+        books =localStorage.getItem('books').split(',');
+    }
+    for(let book of books){
+        const li = document.createElement('li');
+        const spanName = document.createElement('span');
+        spanName.className = 'name';
+        spanName.textContent = book;
+        li.appendChild(spanName);
+        li.innerHTML += spanDelete;
+        ul.appendChild(li);
     }
     e.preventDefault();
 });
+// remove books with delete button
+ul.addEventListener('click',(e)=>{
+    if(e.target.className ==='delete'){
+        e.target.parentElement.remove();
+        removeFromLocalStorage(e.target.parentElement.firstElementChild.textContent);
+    }
+    
+    
+    
+    e.preventDefault();
+});
+
+
+// functions
+function storeTOlocalStorage(book){
+    let books;
+    if(localStorage.getItem('books') === null){
+        books =[];
+    } else {
+        books =localStorage.getItem('books').split(',');
+    }
+    books.push(book);
+    localStorage.setItem("books",books);
+}
+function removeFromLocalStorage(book){
+    let books;
+    if(localStorage.getItem('books') === null){
+        books =[];
+    } else {
+        books =localStorage.getItem('books').split(',');
+    }
+    console.log(books);
+    for(let i=0;i<books.length;i++){
+        if(books[i]===book){
+            books.splice(i,1);
+        }
+    }
+    console.log(books);
+    if(books.length ===0){
+        localStorage.clear();
+    } else {
+        localStorage.setItem('books',books);
+    }
+}
